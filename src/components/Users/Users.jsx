d46929e97c1cc72f1c22e2b245a0de62e8a94f6b
Dimitar <dimitar_dimitrov12@mail.bg>
2020-10-14 18:55:34 +0300

    add axios.create, DAL(data-access layer)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../assets/images/user.png';
-import {NavLink} from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
+// TODO: SERVER GET/POST/CRUD(CREATE_READE_UPDATE_DELETE);
+import * as axios from 'axios';
 function Users(props) {
+  //! Разделяме всичките абонати на колко ще се покзват на една страница и получаме страниците колко ще бъдат на брой. Настройката на pageSize(брой хора на една страница е правим в  user-reducer).
+
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
   let pages = [];
 
@@ -35,13 +38,32 @@ function Users(props) {
           <span>
             <div className={styles.usersPhoto}>
               <NavLink to={'/profile/' + u.id}>
-                  <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
               </NavLink>
             </div>
+
             <div>
               {u.followed ? (
                 <button
                   onClick={() => {
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/
+                        ${u.id}`,
+
+                        {
+                          withCredentials: true,
+                          headers: {
+                            'API-KEY': '64c581f4-8246-4c7e-b42d-06402768460a',
+                          },
+                        },
+                      )
+
+                      .then((response) => {
+                        if (response.data.resultCode == 0) {
+                          props.follow(u.id);
+                        }
+                      });
                     props.unfollow(u.id);
                   }}>
                   Unfollow
@@ -49,7 +71,24 @@ function Users(props) {
               ) : (
                 <button
                   onClick={() => {
-                    props.follow(u.id);
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/
+                        ${u.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers: {
+                            'API-KEY': '64c581f4-8246-4c7e-b42d-06402768460a',
+                          },
+                        },
+                      )
+
+                      .then((response) => {
+                        if (response.data.resultCode == 0) {
+                          props.follow(u.id);
+                        }
+                      });
                   }}>
                   Follow
                 </button>
@@ -75,3 +114,5 @@ function Users(props) {
 }
 
 export default Users;
+// TODO:Създаме два бутона за follow unfollow връзваме ги към сървъра и всеки път
+//TODO: катоискаме да follow или unfollow от правяме въпрос до сървъра ключовете се генерират от account на сървъра API-KEY
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,7 @@ import {
   toggleIsFetching,
   unfollow,
 } from '../../redux/users-reducer';
+import { getUsers } from '../../api/api';
 
 // TODO: SERVER GET
 import * as axios from 'axios';
@@ -23,38 +24,27 @@ class UsersContainer extends Component {
     this.props.toggleIsFetching(true);
 
     // TODO: Въпрос на сървъра
+    getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(data.items);
 
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-      )
-
-      .then((response) => {
-        this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-
-        this.props.setTotalUsersCount(response.data.totalCount);
-      });
+      this.props.setTotalUsersCount(data.totalCount);
+    });
   }
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
     this.props.toggleIsFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-      )
-
-      .then((response) => {
-        this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-      });
+    getUsers(pageNumber, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(data.items);
+    });
   };
 
   render() {
     return (
       <>
-        {/*{this.props.isFetching ? <Preloader /> : null}*/}
+        {this.props.isFetching ? <Preloader /> : null}
         <Users
           totalUsersCount={this.props.totalUsersCount}
           currentPage={this.props.currentPage}
